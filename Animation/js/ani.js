5dffaedf0d786517a9f0a23d801d005a2b01ec7a
ajfunelas <31107247+ajfunelas@users.noreply.github.com>
2018-11-29 07:34:32 +0800

    Add files via upload

---
+++ Animation/js/ani.js
@@ -0,0 +1,8 @@
+var lineDrawing = anime({
+  targets: '#lineDrawing ',
+  strokeDashoffset: [anime.setDashoffset, 0],
+  easing: 'easeInOutSine',
+  duration: 1500,
+  delay: function(el, i) { return i * 250 },
+  direction: 'alternate',
+});
